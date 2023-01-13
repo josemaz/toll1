4a3ebf2e78280cb77e804c5748fa98edf6cc3f82
josemaz <47672759+josemaz@users.noreply.github.com>
2023-01-13 00:20:20 -0600

    Update run.sh

--- prod/run.sh
+++ prod/run.sh
@@ -1,3 +1,6 @@
 NAMD=/opt/apps/namd3/namd3
 
-$NAMD +p 52 +setcpuaffinity MD.conf &> md.out
+# rm -rf !(run.sh|MD.conf|files)
+$NAMD +p 52 +setcpuaffinity  MD.conf &> md.out
+#$NAMD +p 52 +setcpuaffinity +devices 0 MD.conf &> md.out
+#$NAMD +p 52 +setcpuaffinity --CUDASOAintegrate on +devices 0 MD.conf &> md.out
