afffa403fd4b0b35cd18a2b985e43069d1b5ab1e
josemaz <47672759+josemaz@users.noreply.github.com>
2023-01-18 00:18:00 -0600

    Update run.sh

--- prod/run.sh
+++ prod/run.sh
@@ -1,6 +1,9 @@
-NAMD=/opt/apps/namd3/namd3
-
-# rm -rf !(run.sh|MD.conf|files)
-$NAMD +p 52 +setcpuaffinity  MD.conf &> md.out
-#$NAMD +p 52 +setcpuaffinity +devices 0 MD.conf &> md.out
-#$NAMD +p 52 +setcpuaffinity --CUDASOAintegrate on +devices 0 MD.conf &> md.out
+ NAMD=/opt/apps/namd3/namd3
+ 
+ # rm -rf !(run.sh|MD.conf|files|ns_per_day.py)
+ $NAMD +p 52 +setcpuaffinity  MD.conf &> md.out
+ #$NAMD +p 52 +setcpuaffinity +devices 0 MD.conf &> md.out
+ #$NAMD +p 52 +setcpuaffinity --CUDASOAintegrate on +devices 0 MD.conf &> md.  out
+ 
+ 
+ # watch python3 ns_per_day.py md.out
